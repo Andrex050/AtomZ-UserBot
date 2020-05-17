fcd89ca4377baf5ae894faa39937053f164e7120
rashtion <64362298+rashtion@users.noreply.github.com>
2020-05-17 23:24:59 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,10 +6,10 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ATOM"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**Master, ATOM Here ♥️**\n\n"
+    await alive.edit("**Merhaba sahip, ATOM burada ♥️**\n\n"
                      
