3415cee026d97e3a8a812a71a759a08911bf8b37
rashtion <64362298+rashtion@users.noreply.github.com>
2020-05-17 22:09:45 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,13 +11,5 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**Hi sensei, ATOM here! <3**\n\n"
-                     "`☞Telethon version: 1.11.3\n`"
-                     "`☞Python: 3.8.2\n`"
-                     "`☞Bot was modified by:` rashtion\n"
-                     "`☞and created by :` cbgng\n"
-                     "`☞Database Status: Databases functioning normally!\n\n`"
-                     "`☞Türkce dil destegi cok yakinda!!\n`"
-                     f"`☞GitHub Repo r`: [{DEFAULTUSER}](https://github.com/rashtion/AtomZ-UserBot)\n"
-                     #"[Deploy this userbot Now](https://github.com/rashtion/AtomZ-UserBot)"
-                    )
+    await alive.edit("**Master, ATOM Here ♥️**\n\n"
+                     
